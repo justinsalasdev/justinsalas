0389d01557d438e58378c3001d341100ad36960c
l0ckstreet <rsalas7793@gmail.com>
2020-09-09 23:17:40 +0800

    env files

--- pages/api/storeEmail.js
+++ pages/api/storeEmail.js
@@ -8,7 +8,7 @@ handler.post(async (req, res) => {
 	try {
 		const emails = req.db.collection("emails")
 		await emails.insertOne(req.body)
-		res.status(200).json({
+		res.json({
 			info: {
 				message: "Email saved successfully",
 				type: "save",
